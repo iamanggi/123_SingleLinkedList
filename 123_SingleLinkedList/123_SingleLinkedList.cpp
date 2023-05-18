83fedb09f19555c4850970460901686779ee0c87
iamanggi <anggipuspita452@gmail.com>
2023-05-18 15:19:56 +0700

    melakukan methode mencari data

--- 123_SingleLinkedList/123_SingleLinkedList.cpp
+++ 123_SingleLinkedList/123_SingleLinkedList.cpp
@@ -49,3 +49,10 @@ void addNode() {
 	nodeBaru->next = current;
 	previous->next = nodeBaru;
 }
+
+bool searchNode(int nim, Node* current, Node* previous) 
+{
+	previous = START;
+	current = START;
+	
+}
