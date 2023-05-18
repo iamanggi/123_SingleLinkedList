e449ea67a4acf1ed78e56c98aac96e2f7faeb79f
iamanggi <anggipuspita452@gmail.com>
2023-05-18 15:11:02 +0700

    membuat methode menambahkan data

--- 123_SingleLinkedList/123_SingleLinkedList.cpp
+++ 123_SingleLinkedList/123_SingleLinkedList.cpp
@@ -7,3 +7,33 @@ struct Node {
 	Node* next;
 
 };
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan Nim : ";
+	cin >> nim;
+	cout << "Masukan Nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
+
+}
