ea0a629fb6279ec3cfb05e26802c85a846e62338
iamanggi <anggipuspita452@gmail.com>
2023-05-18 15:47:47 +0700

    membuat methode search data dengan while

--- 123_SingleLinkedList/123_SingleLinkedList.cpp
+++ 123_SingleLinkedList/123_SingleLinkedList.cpp
@@ -118,5 +118,13 @@ void searchData() {
 		cout << "Masukan NIM: ";
 		cin >> nim;
 		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
 	}
 }
