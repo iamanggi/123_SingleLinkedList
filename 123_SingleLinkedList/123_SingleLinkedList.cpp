fd6ab7bd6e43b1c6d26971bd3625e9d3e37fff38
iamanggi <anggipuspita452@gmail.com>
2023-05-18 15:38:28 +0700

    membuat method traverse untuk membaca data atau node

--- 123_SingleLinkedList/123_SingleLinkedList.cpp
+++ 123_SingleLinkedList/123_SingleLinkedList.cpp
@@ -89,3 +89,15 @@ bool listEmpty() {
 	else
 		return false;
 }
+
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+	}
+}
